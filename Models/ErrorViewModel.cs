b647a31d6e0fdb78aa304c9b7bb20b707004ac22
StazUnknown <cjloriega67@gmail.com>
2025-10-13 20:36:43 +0800

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Amazing_Barley_By_CjYan.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
